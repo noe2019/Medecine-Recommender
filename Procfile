a090e50762c2141992e2791443aa3e8fd12bdebc
noe2019 <noe.fouotsa@aims-cameroon.org>
2024-11-19 11:48:37 -0500

    Set port environment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:8000 main:app
+web: gunicorn --timeout 120 app:app
--- app.py
+++ app.py
@@ -316,4 +316,7 @@ def predict():
     )
 
 if __name__ == "__main__":
+    # Bind to the PORT environment variable if defined, otherwise default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
